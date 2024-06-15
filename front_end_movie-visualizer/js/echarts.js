385ab281e396c4af58e3de9b1de536239b31a9f3
Binfinity <2870639124@qq.com>
2024-06-15 10:40:57 +0800

    1

--- front_end_movie-visualizer/js/echarts.js
+++ front_end_movie-visualizer/js/echarts.js
@@ -3,7 +3,6 @@ $(function () {
 
     echarts_1();
     echarts_2();
-    map();
     echarts_3();
     echarts_4();
     echarts_5();
@@ -255,129 +254,6 @@ $(function () {
             myChart.resize();
         });
     }
-    function map() {
-        // 基于准备好的dom，初始化echarts实例
-        var myChart = echarts.init(document.getElementById('map'));
-        var data = [
-            { name: '海门', value: 9 },
-
-        ];
-        var geoCoordMap = {
-            '海门': [121.15, 31.89],
-   
-        };
-        var convertData = function (data) {
-            var res = [];
-            for (var i = 0; i < data.length; i++) {
-                var geoCoord = geoCoordMap[data[i].name];
-                if (geoCoord) {
-                    res.push({
-                        name: data[i].name,
-                        value: geoCoord.concat(data[i].value)
-                    });
-                }
-            }
-            return res;
-        };
-
-        option = {
-            // backgroundColor: '#404a59',
-            /***  title: {
-                  text: '实时行驶车辆',
-                  subtext: 'data from PM25.in',
-                  sublink: 'http://www.pm25.in',
-                  left: 'center',
-                  textStyle: {
-                      color: '#fff'
-                  }
-              },**/
-            tooltip: {
-                trigger: 'item'
-            },
-
-            geo: {
-                map: 'china',
-                label: {
-                    emphasis: {
-                        show: false
-                    }
-                },
-                roam: true,
-                itemStyle: {
-                    normal: {
-                        areaColor: '#3eabff',
-                        borderColor: '#fff'
-                    },
-                    emphasis: {
-                        areaColor: '#006be4'
-                    }
-                }
-            },
-            series: [
-                {
-                    name: '实时行驶车辆',
-                    type: 'scatter',
-                    coordinateSystem: 'geo',
-                    data: convertData(data),
-                    symbolSize: function (val) {
-                        return val[2] / 10;
-                    },
-                    label: {
-                        normal: {
-                            formatter: '{b}',
-                            position: 'right',
-                            show: false
-                        },
-                        emphasis: {
-                            show: true
-                        }
-                    },
-                    itemStyle: {
-                        normal: {
-                            color: '#fff'
-                        }
-                    }
-                },
-                {
-                    name: 'Top 5',
-                    type: 'effectScatter',
-                    coordinateSystem: 'geo',
-                    data: convertData(data.sort(function (a, b) {
-                        return b.value - a.value;
-                    }).slice(0, 6)),
-                    symbolSize: function (val) {
-                        return val[2] / 20;
-                    },
-                    showEffectOn: 'render',
-                    rippleEffect: {
-                        brushType: 'stroke'
-                    },
-                    hoverAnimation: true,
-                    label: {
-                        normal: {
-                            formatter: '{b}',
-                            position: 'right',
-                            show: true
-                        }
-                    },
-                    itemStyle: {
-                        normal: {
-                            color: '#0041d2',
-                            shadowBlur: 10,
-                            shadowColor: 'rgba(0,0,0,.3)'
-                        }
-                    },
-                    zlevel: 1
-                }
-            ]
-        };
-
-        myChart.setOption(option);
-        window.addEventListener("resize", function () {
-            myChart.resize();
-        });
-    }
-
     function echarts_3() {
         // 基于准备好的dom，初始化echarts实例
         var myChart = echarts.init(document.getElementById('echarts_3'));
